fbd2a87d2dfe40837a0fcdf7b293791dcc0f97b0
kuthalingaraj S <90622742+Kuthalingaraj@users.noreply.github.com>
2022-04-28 19:25:52 +0530

    Add files via upload

---
+++ Demomethod.java
@@ -0,0 +1,10 @@
+public class Demomethod{
+    static void myMethod(){
+        System.out.println("Zoho");
+    }
+    public static void main(String[] args){
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
